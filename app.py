84f71b09aaafd4e3496045e736ca23423218f926
santongding <santongding@foxmail.com>
2023-12-07 21:14:19 +0800

    feat: Support multiple client

--- app.py
+++ app.py
@@ -2,38 +2,24 @@ from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
 import cv2
 import time
+import threading
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+cond = threading.Condition()
+rsc = None
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-camera = cv2.VideoCapture(0)
-@app.route('/')
-def index():
-    # 主页面
-    return render_template('index.html')
-
-def gen_frames():  
-    fps = 0
-    frame_count = 0
-    start_time = time.time()
+def gen_frames_async():
+    global rsc
+    print("entering generate thread")
+    camera = cv2.VideoCapture(0)
     while True:
-        global camera
         success, frame = camera.read()  # read the camera frame
         if not success:
             print("Can not read from camera")
             time.sleep(1)
         else:
-            frame_count += 1
-            end_time = time.time()
-            if (end_time - start_time) > 1:
-                fps = frame_count / (end_time - start_time)
-                frame_count = 0
-                start_time = time.time()
-                
-                # Emit FPS to client via socket.io
-                socketio.emit('FPS', {'fps': fps})
-            
-                        # Convert to grayscale for face detection
+                                  # Convert to grayscale for face detection
             resize_scale = 0.3
             small_frame = cv2.resize(frame,(0, 0), fx=resize_scale, fy=resize_scale)
             gray = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
@@ -47,9 +33,38 @@ def gen_frames():
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
+            with cond:
+                rsc = (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
+                cond.notify_all()
 
+@app.route('/')
+def index():
+    # 主页面
+    return render_template('index.html')
+
+def gen_frames():  
+    fps = 0
+    frame_count = 0
+    start_time = time.time()
+    last_frm = None
+    global rsc
+    while True:
+            # Emit FPS to client via socket.io
+        with cond:
+            if rsc == last_frm:
+                cond.wait()
+            last_frm = rsc
+        assert not last_frm is None
+        frame_count += 1
+        end_time = time.time()
+        if (end_time - start_time) > 1:
+            fps = frame_count / (end_time - start_time)
+            frame_count = 0
+            start_time = time.time()
+        socketio.emit('FPS', {'fps': fps})
+        yield last_frm
+            
 @app.route('/video_feed')
 def video_feed():
     # 视频流路由
@@ -57,5 +72,8 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
+    thread = threading.Thread(target=gen_frames_async)
+    # Start the thread
+    thread.start()
     # 开始 Flask 应用
-    socketio.run(app, use_reloader=False, debug=True, port=5003)
+    socketio.run(app, use_reloader=False, debug=True, port=5001)
